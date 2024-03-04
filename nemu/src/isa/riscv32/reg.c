c76b8300b51638af243206ad7333e14b4cbbe38d
tracer-ics2023 <Penghui@Linux>
2024-03-04 14:38:45 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:38:45 up 1 day, 7 min, 13 users,  load average: 0.10, 0.13, 0.16

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -28,6 +28,7 @@ const char *regs[] = {
 // gpr is the register of NEMU, it located /nemu/src/isa/riscv32/include/isa-def.h
 void isa_reg_display() {
 	int i;	
+	printf("%u\n", cpu.pc);
 	for (i = 0; i < REG_NUM; i++)
 	{
 		printf("$%s\t0x%08d\n", regs[i], cpu.gpr[i]);

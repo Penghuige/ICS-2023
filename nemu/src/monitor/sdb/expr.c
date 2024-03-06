4a6df2c793b2356eafc70b0079817965a6a0e982
tracer-ics2023 <Penghui@Linux>
2024-03-06 15:33:38 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:33:38 up 1 day, 10:31, 12 users,  load average: 0.38, 0.31, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -68,8 +68,9 @@ void init_regex() {
 
   for (i = 0; i < NR_REGEX; i ++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
-		//printf("%d ", ret);
+		printf("%d ", ret);
     if (ret != 0) {
+			continue;
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }

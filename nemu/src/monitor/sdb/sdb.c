1987640a1c65346a35beff22a7308aafa629efa4
tracer-ics2023 <Penghui@Linux>
2024-03-17 12:43:21 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:43:21 up 2 days,  3:18, 13 users,  load average: 0.06, 0.12, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -230,7 +230,7 @@ void sdb_mainloop() {
 		// printf("hello?\n");
 		char* res = strtok(test, " ");
 		uint32_t resNum = atoi(res);
-		char* t2 = strtok(NULL, " ");	
+		char* t2 = strtok(res, " ");	
 		cmd_p(t2);
 		if(resNum == -1) return;
 	}

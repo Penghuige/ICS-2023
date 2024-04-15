333a0921cd2a731bdf9de4d4f04bb28b308e68a5
tracer-ics2023 <Penghui@Linux>
2024-04-15 19:17:11 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:17:11 up 2 days, 11:48, 13 users,  load average: 0.40, 0.32, 0.32

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -320,6 +320,7 @@ uint32_t eval(uint32_t p, uint32_t q) {
 			if(ju) continue;
 			if(pri[tokens[i].type] >= Max)
 			{
+				printf("%d:%d\n", i, tokens[i].type);
 				op = i;
 				Max = pri[tokens[i].type];
 			}

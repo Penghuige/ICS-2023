b425074927f60b1a56e3d8be0e8266c2c527b46c
tracer-ics2023 <Penghui@Linux>
2024-04-17 09:17:58 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:17:58 up 2 days, 17:51, 14 users,  load average: 0.22, 0.29, 0.27

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -42,6 +42,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
 	int num = check_wp();
+	printf("num:%d\n", num);
 	//printf("hello!\n");;
 	if(num != 0)
 	{

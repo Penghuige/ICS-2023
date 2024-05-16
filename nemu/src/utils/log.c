3a12fc28060e8d56d9db5798df6a7ceba46810e6
tracer-ics2023 <Penghui@Linux>
2024-05-16 15:54:27 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:27 up 2 days, 50 min, 12 users,  load average: 0.48, 0.33, 0.28

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -24,7 +24,6 @@ void init_log(const char *log_file) {
   log_fp = stdout;
   if (log_file != NULL) {
     FILE *fp = fopen(log_file, "w");
-		printf("\n open %s\n\n", log_file);
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;
   }

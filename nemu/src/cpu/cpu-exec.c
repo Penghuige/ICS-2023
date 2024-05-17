369aa750a48a8e1c8fd88deacfdeea20d09f1406
tracer-ics2023 <Penghui@Linux>
2024-05-17 11:15:32 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:15:32 up 2 days,  2:39, 12 users,  load average: 0.24, 0.27, 0.30

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -123,16 +123,7 @@ void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
-			for(int i = 0; i < MAX_RING_TO_STORE; i++)
-			{
-				char a[4] = "   ";
-				if(i == ring_cnt)
-				{
-					strcpy(a, "==>");
-				}
-				printf("%s%s\n", a, ring_buffer[i]);
-			}
-      Log("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
       return;
     default: nemu_state.state = NEMU_RUNNING;
   }
@@ -148,6 +139,15 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
+			for(int i = 0; i < MAX_RING_TO_STORE; i++)
+			{
+				char a[4] = "   ";
+				if(i == ring_cnt)
+				{
+					strcpy(a, "==>");
+				}
+				Log("%s%s\n", a, ring_buffer[i]);
+			}
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :

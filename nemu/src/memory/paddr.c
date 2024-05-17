bfd4dd4e4303c1568c0d87d165c8186ee67c0697
tracer-ics2023 <Penghui@Linux>
2024-05-17 11:32:48 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:32:48 up 2 days,  2:56, 12 users,  load average: 0.28, 0.27, 0.27

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -59,7 +59,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-	Log("physical memory write %08x, data is %032u", addr, data);
+	Log("physical memory write %08x, data is %u", addr, data);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);

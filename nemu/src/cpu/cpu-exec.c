77f9255f04a666835a53aa7be634a9349c48b069
tracer-ics2023 <Penghui@Linux>
2024-05-22 09:24:37 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:24:37 up 2 days, 10:00, 13 users,  load average: 0.42, 0.45, 0.49

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -139,6 +139,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
+#ifdef CONFIG_ITRACE_COND
 			for(int i = 0; i < MAX_RING_TO_STORE; i++)
 			{
 				char a[4] = "   ";
@@ -148,6 +149,7 @@ void cpu_exec(uint64_t n) {
 				}
 				Log("%s%s", a, ring_buffer[i]);
 			}
+#endif
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :

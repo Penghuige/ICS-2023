cf7c08dbe989461e15d80d4f3ac2a357a04d617e
tracer-ics2023 <Penghui@Linux>
2024-05-24 12:02:38 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:38 up 2 days, 18:40, 13 users,  load average: 0.32, 0.41, 0.35

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,6 +21,16 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < ARRLEN(ref_r->gpr); i++)
   {
     if(cpu.gpr[i] != ref_r->gpr[i]){
+      // disaplay all the right register
+      extern const char *regs[];
+      for(int j = 0; j < ARRLEN(ref_r->gpr)/4; j++)
+      {
+        for(int k = 0; k < 4; k++)
+        {
+          printf("\e[1;33m$%s\t0x%08x\t\e[0m", regs[4*j+k], ref_r->gpr[4*j+k]);
+        }
+        printf("\n");
+      }
       return false;
     }
   }

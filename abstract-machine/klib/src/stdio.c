b7923ea4453fce6f436bc4b5151de28035a3997e
tracer-ics2023 <Penghui@Linux>
2024-05-25 11:23:00 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:22:59 up 2 days, 23:54, 12 users,  load average: 0.27, 0.28, 0.34

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,74 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  size_t i;
+  size_t j = 0;
+
+  va_list ap;
+  char *s;
+  int d;
+  char buff[20];
+  size_t t;
+
+  va_start(ap, fmt);
+  
+  for(i = 0; fmt[i] != '\0'; i++)
+  {
+    if(fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case '%':
+          out[j++] = '%';
+          break;
+        case 's':
+          s = va_arg(ap, char *);
+          for(t = 0; s[t] != '\0'; t++)
+          {
+            out[j++] = s[t];
+          }
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          //itoa(d, buff, 10);
+          t = 0;
+          if(d < 0)
+          {
+            buff[t++] = '-';
+          }
+          else if(d == 0)
+          {
+            buff[t++] = '0'; 
+          }
+          while(d != 0)
+          {
+            buff[t++] = d % 10 + '0';
+            d /= 10;
+          }
+          size_t tmp = (d < 0) ? 1 : 0;
+          for(tmp = 0; tmp < t / 2; tmp++)
+          {
+            char c = buff[t-tmp-1];
+            buff[t-tmp-1] = buff[tmp];
+            buff[tmp] = c;
+          }
+          buff[t] = '\0';
+
+          for(t = 0; buff[t] != '\0'; t++)
+          {
+            out[j++] = buff[t];
+          }
+          break;
+      }
+    }
+    else
+    {
+      out[j++] = fmt[i];
+    }
+  }
+  out[j] = '\0';
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

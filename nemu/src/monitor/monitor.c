94deb19da6ecf2a64e3bac66c4fce80d6e3901c2
tracer-ics2023 <Penghui@Linux>
2024-05-27 16:02:54 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:02:54 up 3 days,  3:38, 12 users,  load average: 0.17, 0.12, 0.17

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -79,18 +79,18 @@ static int parse_args(int argc, char *argv[]) {
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
-    {"ftrace"   , required_argument, NULL, 'a'},
+    {"ftrace"   , required_argument, NULL, 't'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhlt:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'a': elf_file = optarg; break;
+      case 't': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -98,7 +98,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        printf("\t-a,--ftrace=FILE        run ftrace\n");
+        printf("\t-t,--ftrace=FILE        run ftrace\n");
         printf("\n");
         exit(0);
     }

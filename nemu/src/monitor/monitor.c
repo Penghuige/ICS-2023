1f7967c8823c1060a2b6c7dbb6329203f75b8af1
tracer-ics2023 <Penghui@Linux>
2024-05-27 18:44:59 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:59 up 3 days,  5:29, 12 users,  load average: 0.25, 0.33, 0.44

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -221,7 +221,7 @@ static int parse_args(int argc, char *argv[]) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      case 'l': log_file = optarg; break;
+      case 'l': printf("hello?\n");log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 't': printf("hello?\n"); elf_file = optarg; initial_table(); break;
       case 1: img_file = optarg; return 0;

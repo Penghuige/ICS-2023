d6990a3e64c45991b5015a45c7b93e4d4052e688
tracer-ics2023 <Penghui@Linux>
2024-05-28 15:14:34 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:14:34 up 3 days,  7:51, 12 users,  load average: 0.35, 0.30, 0.31

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -81,7 +81,7 @@ static void ftrace_record(Decode *s)
         }
       }
       assert(sym2 != NULL);
-      printf("[0x%8x] From %s(0x%8x) to %s(0x%8x).\n", \
+      printf("\033[0;31m[0x%8x] From %s(0x%8x) to %s(0x%8x).\033[0m\n", \
           s->pc, &strtab[sym2->st_name], s->pc, &strtab[sym->st_name], sym->st_value);
       break;
     }

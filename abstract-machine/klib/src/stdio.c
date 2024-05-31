7872dcfe218a1a95197932c95a813450ec15aad5
tracer-ics2023 <Penghui@Linux>
2024-05-31 15:00:44 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:44 up 3 days, 22:48, 13 users,  load average: 0.85, 0.97, 1.44

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,76 +4,92 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
 int printf(const char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    char buff[20];
-    
-    size_t i = 0;
-    size_t j = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  char buff[20] = {0};
+  
+  //char out[2000] = {0};
+  size_t i;
+  size_t j = 0;
 
-    char *s;
-    char c;
-    int d;
-    size_t t;
+  char *s;
+  char c;
+  int d;
+  size_t t;
 
-    for (i = 0; fmt[i] != '\0'; i++) {
-        if (fmt[i] == '%') {
-            i++;
-            switch (fmt[i]) {
-                case 'd':
-                    d = va_arg(ap, int);
-                    if (d < 0) {
-                        putch('-');
-                        d = -d;
-                        j++;
-                    }
-                    t = 0;
-                    if (d == 0) {
-                        buff[t++] = '0';
-                    } else {
-                        while (d != 0) {
-                            buff[t++] = (d % 10) + '0';
-                            d /= 10;
-                        }
-                        // Reverse the buffer
-                        for (size_t k = 0; k < t / 2; k++) {
-                            char tmp = buff[k];
-                            buff[k] = buff[t - k - 1];
-                            buff[t - k - 1] = tmp;
-                        }
-                    }
-                    buff[t] = '\0';
-                    for (size_t k = 0; buff[k] != '\0'; k++) {
-                        putch(buff[k]);
-                        j++;
-                    }
-                    break;
-                case 's':
-                    s = va_arg(ap, char *);
-                    for (t = 0; s[t] != '\0'; t++) {
-                        putch(s[t]);
-                        j++;
-                    }
-                    break;
-                case 'c':
-                    c = va_arg(ap, int);
-                    putch(c);
-                    j++;
-                    break;
-                default:
-                    putch('%');
-                    putch(fmt[i]);
-                    j += 2;
-                    break;
-            }
-        } else {
-            putch(fmt[i]);
-            j++;
-        }
+  for(i = 0; fmt[i] != '\0'; i++)
+  {
+    if(fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case 'd':
+          d = va_arg(ap, int);
+          //itoa(d, buff, 10);
+          t = 0;
+          size_t tmp = (d < 0) ? 1 : 0;
+          if(d < 0)
+          {
+            buff[t++] = '-';
+            d = -d;
+          }
+          else if(d == 0)
+          {
+            buff[t++] = '0'; 
+          }
+          while(d != 0)
+          {
+            buff[t++] = d % 10 + '0';
+            d /= 10;
+          }
+          for( ; tmp < t / 2; tmp++)
+          {
+            char c = buff[t-tmp-1];
+            buff[t-tmp-1] = buff[tmp];
+            buff[tmp] = c;
+          }
+          buff[t] = '\0';
+
+          for(t = 0; buff[t] != '\0'; t++)
+          {
+            //out[j++] = buff[t];
+            putch(buff[t]);
+          }
+          break;
+        case 's':
+          s = va_arg(ap, char *);
+          for(t = 0; s[t] != '\0'; t++)
+          {
+            //out[j++] = s[t];
+            putch(s[t]);
+          }
+          break;
+        case 'c':
+          c = va_arg(ap, int);
+          putch(c);
+          break;
+        default:
+          break;
+      }
     }
-    va_end(ap);
-    return (int)j;
+    else
+    {
+      //out[j++] = fmt[i];
+      putch(fmt[i]);
+    }
+  }
+  //out[j] = '\0';
+  
+
+  //for(i = 0; out[i] != '\0'; i++)
+  //{
+  //  putch(out[i]);
+  //}
+  va_end(ap);
+  return (int)j;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {

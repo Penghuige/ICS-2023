72c3636a380307e4491d1b7c7b53fddf2e2d605a
tracer-ics2023 <Penghui@Linux>
2024-05-31 12:41:35 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:41:35 up 3 days, 21:56, 13 users,  load average: 0.66, 0.62, 0.41

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,24 +6,100 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  char buff[20] = {0};
+  
+  //char out[2000] = {0};
   size_t i;
   size_t j = 0;
 
-  va_list ap;
+  char *s;
+  char c;
+  int d;
+  size_t t;
+
+  for(i = 0; fmt[i] != '\0'; i++)
+  {
+    if(fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case 'd':
+          d = va_arg(ap, int);
+          //itoa(d, buff, 10);
+          t = 0;
+          size_t tmp = (d < 0) ? 1 : 0;
+          if(d < 0)
+          {
+            buff[t++] = '-';
+            d = -d;
+          }
+          else if(d == 0)
+          {
+            buff[t++] = '0'; 
+          }
+          while(d != 0)
+          {
+            buff[t++] = d % 10 + '0';
+            d /= 10;
+          }
+          for( ; tmp < t / 2; tmp++)
+          {
+            char c = buff[t-tmp-1];
+            buff[t-tmp-1] = buff[tmp];
+            buff[tmp] = c;
+          }
+          buff[t] = '\0';
+
+          for(t = 0; buff[t] != '\0'; t++)
+          {
+            //out[j++] = buff[t];
+            putch(buff[t]);
+          }
+          break;
+        case 's':
+          s = va_arg(ap, char *);
+          for(t = 0; s[t] != '\0'; t++)
+          {
+            //out[j++] = s[t];
+            putch(s[t]);
+          }
+          break;
+        case 'c':
+          c = va_arg(ap, int);
+          putch(c);
+          break;
+        default:
+          break;
+      }
+    }
+    else
+    {
+      //out[j++] = fmt[i];
+      putch(fmt[i]);
+    }
+  }
+  //out[j] = '\0';
+  
+
+  //for(i = 0; out[i] != '\0'; i++)
+  //{
+  //  putch(out[i]);
+  //}
+  va_end(ap);
+  return (int)j;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  size_t i;
+  size_t j = 0;
+
   char *s;
   int d;
   char buff[20];
   size_t t;
-
-  va_start(ap, fmt);
   
   for(i = 0; fmt[i] != '\0'; i++)
   {
@@ -84,13 +160,211 @@ int sprintf(char *out, const char *fmt, ...) {
   out[j] = '\0';
   return strlen(out);
 }
+int sprintf(char *out, const char *fmt, ...) {
+  size_t i = 0;
+  size_t j = 0;
+
+  va_list ap;
+  va_start(ap, fmt);
+
+  while (fmt[i] != '\0') {
+    if (fmt[i] == '%') {
+      i++;
+      switch (fmt[i]) {
+        case '%':
+          out[j++] = '%';
+          break;
+        case 's': {
+          char *s = va_arg(ap, char *);
+          while (*s) {
+            out[j++] = *s++;
+          }
+          break;
+        }
+        case 'd': {
+          int d = va_arg(ap, int);
+          char buff[12]; // Enough to hold all digits of a 32-bit integer, including sign and null terminator
+          int t = 0;
+          if (d < 0) {
+            out[j++] = '-';
+            d = -d;
+          }
+          if (d == 0) {
+            buff[t++] = '0';
+          } else {
+            while (d > 0) {
+              buff[t++] = (d % 10) + '0';
+              d /= 10;
+            }
+            for (int k = 0; k < t / 2; k++) {
+              char temp = buff[k];
+              buff[k] = buff[t - 1 - k];
+              buff[t - 1 - k] = temp;
+            }
+          }
+          buff[t] = '\0';
+          for (int k = 0; buff[k] != '\0'; k++) {
+            out[j++] = buff[k];
+          }
+          break;
+        }
+        default:
+          // Unsupported format specifier
+          out[j++] = '%';
+          out[j++] = fmt[i];
+          break;
+      }
+    } else {
+      out[j++] = fmt[i];
+    }
+    i++;
+  }
+  out[j] = '\0';
+  va_end(ap);
+  return j;
+}
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  size_t i;
+  size_t j = 0;
+
+  va_list ap;
+  char *s;
+  int d;
+  char buff[20];
+  size_t t;
+
+  va_start(ap, fmt);
+  
+  for(i = 0; i < n && fmt[i] != '\0'; i++)
+  {
+    if(fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case '%':
+          out[j++] = '%';
+          break;
+        case 's':
+          s = va_arg(ap, char *);
+          for(t = 0; s[t] != '\0'; t++)
+          {
+            out[j++] = s[t];
+          }
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          //itoa(d, buff, 10);
+          t = 0;
+          size_t tmp = (d < 0) ? 1 : 0;
+          if(d < 0)
+          {
+            buff[t++] = '-';
+            d = -d;
+          }
+          else if(d == 0)
+          {
+            buff[t++] = '0'; 
+          }
+          while(d != 0)
+          {
+            buff[t++] = d % 10 + '0';
+            d /= 10;
+          }
+          for( ; tmp < t / 2; tmp++)
+          {
+            char c = buff[t-tmp-1];
+            buff[t-tmp-1] = buff[tmp];
+            buff[tmp] = c;
+          }
+          buff[t] = '\0';
+
+          for(t = 0; buff[t] != '\0'; t++)
+          {
+            out[j++] = buff[t];
+          }
+          break;
+      }
+    }
+    else
+    {
+      out[j++] = fmt[i];
+    }
+  }
+  out[n] = '\0';
+  va_end(ap);
+  return strlen(out);
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  size_t i;
+  size_t j = 0;
+
+  char *s;
+  int d;
+  char buff[20];
+  size_t t;
+
+  
+  for(i = 0; i < n && fmt[i] != '\0'; i++)
+  {
+    if(fmt[i] == '%')
+    {
+      i++;
+      switch (fmt[i])
+      {
+        case '%':
+          out[j++] = '%';
+          break;
+        case 's':
+          s = va_arg(ap, char *);
+          for(t = 0; s[t] != '\0'; t++)
+          {
+            out[j++] = s[t];
+          }
+          break;
+        case 'd':
+          d = va_arg(ap, int);
+          //itoa(d, buff, 10);
+          t = 0;
+          size_t tmp = (d < 0) ? 1 : 0;
+          if(d < 0)
+          {
+            buff[t++] = '-';
+            d = -d;
+          }
+          else if(d == 0)
+          {
+            buff[t++] = '0'; 
+          }
+          while(d != 0)
+          {
+            buff[t++] = d % 10 + '0';
+            d /= 10;
+          }
+          for( ; tmp < t / 2; tmp++)
+          {
+            char c = buff[t-tmp-1];
+            buff[t-tmp-1] = buff[tmp];
+            buff[tmp] = c;
+          }
+          buff[t] = '\0';
+
+          for(t = 0; buff[t] != '\0'; t++)
+          {
+            out[j++] = buff[t];
+          }
+          break;
+      }
+    }
+    else
+    {
+      out[j++] = fmt[i];
+    }
+  }
+  out[n] = '\0';
+  return strlen(out);
 }
 
 #endif

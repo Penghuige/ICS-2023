879d93e08244c9a0f02b0b5945b7b5fc5c79313d
tracer-ics2023 <Penghui@Linux>
2024-05-31 15:35:25 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:35:25 up 3 days, 23:22, 13 users,  load average: 1.34, 0.90, 1.14

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,7 +5,21 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+static char sprint_buf[1024];
+
+int printf(const char *fmt, ...)//可以有一个或多个固定参数
+{
+  va_list args; //用于存放参数列表的数据结构
+  int n;
+  /*根据最后一个fmt来初始化参数列表，至于为什么是最后一个参数，是与va_start有关。*/
+  va_start(args, fmt);
+  n = vsprintf(sprint_buf, fmt, args);
+  va_end(args);//执行清理参数列表的工作
+  putstr(sprint_buf);
+  return n;
+}
+
+int my_printf(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   char buff[20] = {0};

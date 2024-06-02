2eb2eb723d75297e1bebb4f702cf95ecd02f25af
tracer-ics2023 <Penghui@Linux>
2024-06-02 11:27:30 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:30 up 4 days,  6:04, 17 users,  load average: 0.15, 0.18, 0.26

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -69,8 +69,8 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char *d = dst;
-  const unsigned char *s = src;
+  unsigned char *d = (unsigned char *)dst;
+  const unsigned char *s = (unsigned char *)src;
   if (d < s) {
     for (size_t i = 0; i < n; i++) {
       d[i] = s[i];

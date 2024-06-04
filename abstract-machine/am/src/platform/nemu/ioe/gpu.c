d5a724f3cb4318d9f61bcd6d50f51a1ce805f395
tracer-ics2023 <Penghui@Linux>
2024-06-04 16:08:02 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:02 up 4 days, 14:31, 18 users,  load average: 0.74, 0.91, 0.82

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,6 +25,19 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  // get the screen width, avoid to leak
+  uint32_t t = inl(VGACTL_ADDR) >> 16;
+
+  uint32_t *p = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (int i = y; i < y + h; i ++)
+  {
+    for(int j = x; j < x + w; j++)
+    {
+      fb[x + y * t] = p[x + y * t];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }

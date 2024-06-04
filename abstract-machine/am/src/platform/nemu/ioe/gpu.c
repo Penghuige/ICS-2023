d33c2e107d142d2aca317000b5774f46ae850ff4
tracer-ics2023 <Penghui@Linux>
2024-06-04 16:14:57 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:57 up 4 days, 14:38, 18 users,  load average: 1.29, 1.02, 0.91

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -37,8 +37,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   {
     for(int j = x; j < x + w; j++)
     {
-      //fb[i*t+j] = p[i+j*t];
-      fb[t*i+j] = p[w*(i-y)+(j-x)];
+      fb[i*t+j] = p[i+j*t];
+      //fb[t*i+j] = p[w*(i-y)+(j-x)];
     }
   }
   if (ctl->sync) {

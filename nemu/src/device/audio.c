c74a894ebc69fc3144c3701f97fcebe795da2f3d
tracer-ics2023 <Penghui@Linux>
2024-06-05 20:13:34 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:34 up  6:03, 11 users,  load average: 0.84, 1.46, 1.69

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -81,6 +81,6 @@ void init_audio() {
 #endif
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
   audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 }

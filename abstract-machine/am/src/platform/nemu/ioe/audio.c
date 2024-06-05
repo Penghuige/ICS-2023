b1364bc2b28c6e59b4098259b6d8ef8bb7639833
tracer-ics2023 <Penghui@Linux>
2024-06-05 20:04:09 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:04:09 up  5:54, 11 users,  load average: 3.10, 2.25, 1.81

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -15,7 +15,7 @@ void __am_audio_init() {
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   cfg->present = true;
-  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
+  //cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 

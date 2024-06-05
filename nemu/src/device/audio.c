5d91b004a420825ec174b5008cca90e848224b00
tracer-ics2023 <Penghui@Linux>
2024-06-05 20:12:55 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:12:55 up  6:02, 11 users,  load average: 1.15, 1.60, 1.75

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -51,25 +51,24 @@ static void audio_play(void *userdata, uint8_t *stream, int len)
   {
     SDL_MixAudio(stream, sbuf + pos, len , SDL_MIX_MAXVOLUME);
   }
-    pos = (pos + len) % sbuf_size;
+  pos = (pos + len) % sbuf_size;
   audio_base[reg_count] -= len;
 }
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  if(audio_base[reg_init] != 1){
-    return;
+  if(audio_base[reg_init] == 1){
+    audio_base[reg_init] = 0;
+    SDL_AudioSpec s = {};
+    s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    s.callback = audio_play;
+    s.userdata = NULL;        // 不使用
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
   }
-  audio_base[reg_init] = 0;
-  SDL_AudioSpec s = {};
-  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-  s.freq = audio_base[reg_freq];
-  s.channels = audio_base[reg_channels];
-  s.samples = audio_base[reg_samples];
-  s.callback = audio_play;
-  s.userdata = NULL;        // 不使用
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
 }
 
 void init_audio() {

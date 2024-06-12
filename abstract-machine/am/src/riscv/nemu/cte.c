98e1c21e4c7fe06db54b5907a8ca6b6aea607dfd
tracer-ics2023 <Penghui@Linux>
2024-06-12 14:56:25 +0800

    >  compile NEMU Penguhi 蓬灰 Linux no-bug 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:56:25 up 18:06, 15 users,  load average: 0.20, 0.31, 0.27

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,12 +6,14 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
+    for(int i = 0; i < 32; i++) printf("%u\n", c->gpr[i]);
     Event ev = {0};
     switch (c->mcause) {
       default: ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
+    for(int i = 0; i < 32; i++) printf("%u\n", c->gpr[i]);
     assert(c != NULL);
   }
 

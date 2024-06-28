80412d5689c823b20a8b13d85231c7a0afb34c8c
tracer-ics2023 <Penghui@Linux>
2024-06-28 15:41:23 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Mon May 20 15:51:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:41:23 up  5:36,  1 user,  load average: 1.22, 1.11, 0.90

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,13 @@
 #include <common.h>
 #include "syscall.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case EVENT_SYSCALL:
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

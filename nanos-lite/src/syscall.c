d997bd2f7c137ccb67e81e93a49b0f404c826e72
tracer-ics2023 <Penghui@Linux>
2024-06-29 16:13:56 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-35-generic #35-Ubuntu SMP PREEMPT_DYNAMIC Mon May 20 15:51:52 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:13:56 up  4:15,  1 user,  load average: 0.32, 0.41, 0.57

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -25,7 +25,7 @@ void do_syscall(Context *c) {
         case 1: // sys_yield
           sys_yield();
           break;
-        case 5: // sys_write
+        case 4: // sys_write
           printf("%.*s", a[3], (char *)a[2]);
           c->GPRx = a[3];
           break;

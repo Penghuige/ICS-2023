a3153b293d052bcca34c1b593f14d1a1f99746a8
tracer-ics2023 <Penghui@Linux>
2024-07-05 13:34:36 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:34:36 up  4:23,  1 user,  load average: 0.42, 0.67, 0.73

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -37,9 +37,9 @@ typedef struct {
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[11]
-#define GPR3 gpr[12]
-#define GPR4 gpr[13]
+#define GPR2 gpr[10]
+#define GPR3 gpr[11]
+#define GPR4 gpr[12]
 #define GPRx gpr[10]
 
 #endif

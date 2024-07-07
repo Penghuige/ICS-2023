4b025de80a0a9a453cce2203f097021aaabeea45
tracer-ics2023 <Penghui@Linux>
2024-07-07 17:16:59 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:16:59 up  6:52,  1 user,  load average: 1.41, 1.04, 0.87

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -6,7 +6,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
-    printf("%d\n", c->mcause);
+    printf("%x\n", c->mcause);
     Event ev = {0};
     switch (c->mcause) {
       // 是因为在yield函数中，我们调用了ecall指令，这个指令会触发一个异常，异常号为-1。

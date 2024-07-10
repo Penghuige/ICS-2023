82e2b60e9de5b14d55a50f3cddddb021b232309d
tracer-ics2023 <Penghui@Linux>
2024-07-10 15:50:39 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:39 up  5:08,  1 user,  load average: 0.74, 0.56, 0.60

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -14,6 +14,8 @@ static Context* do_event(Event e, Context* c) {
       printf("EVENT_SYSCALL\n");
       do_syscall(c);
       break;
+    case EVENT_ERROR:
+      printf("EVENT_ERROR\n");
     default: panic("Unhandled event ID = %d", e.event);
   }
 

453b1b30d583ca05a59b528ca22a306410f3af1b
tracer-ics2023 <Penghui@Linux>
2024-07-11 17:08:06 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:08:06 up 1 day,  6:25,  1 user,  load average: 0.87, 1.60, 1.82

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,7 +5,7 @@ extern void do_syscall(Context* c);
 static Context* do_event(Event e, Context* c) {
 //#ifdef CONFIG_STRACE
   // the GPR1 is a7 register. It is used to pass the syscall number.  the GPRx is a0 register. It is used to pass the return value.
-  printf("event ID=%d c->GPR1=%d c->GPRx=%d\n",e.event,c->GPR1, c->GPRx);
+  //printf("event ID=%d c->GPR1=%d c->GPRx=%d\n",e.event,c->GPR1, c->GPRx);
 //#endif
   switch (e.event) {
     case EVENT_YIELD:

54d0c65a7bd1b5bbafe9f0d11034089862446bc0
tracer-ics2023 <Penghui@Linux>
2024-07-12 09:54:13 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:54:13 up 1 day, 23:12,  1 user,  load average: 0.71, 0.63, 0.55

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -32,7 +32,6 @@ void do_syscall(Context *c) {
       //sys_write((intptr_t*)a[2], a[3]);
       printf("%s", (char*)a[2]);
       c->GPRx = a[3];
-      asm volatile("ecall");
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -17,7 +17,7 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  if(NO == 0) epc += 4;
+  if(NO == -1) epc += 4;
   //cpu.csr.mcause = NO;
   cpu.csr.mcause = NO;
   // false instruction

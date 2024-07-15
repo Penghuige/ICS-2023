4eca1b2cb1d23e3235fbab1731a75e1389ebcaed
tracer-ics2023 <Penghui@Linux>
2024-07-15 11:57:28 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:57:28 up 5 days,  1:15,  1 user,  load average: 1.27, 1.27, 1.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -86,6 +86,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
+  printf("filename: %s\n", filename);
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();

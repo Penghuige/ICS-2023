62938a00bb3e6cbac8d256aba827c9eed6dd404a
tracer-ics2023 <Penghui@Linux>
2024-07-15 12:10:06 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:10:06 up 5 days,  1:27,  1 user,  load average: 0.92, 1.00, 0.99

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,6 +69,9 @@ static int get_index(int fd)
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
+  for(int i = 0; i < LENGTH(file_table); i++) {
+    printf("file_table[%d].name = %s\n", i, file_table[i].name);
+  }
   Log("open %s\n", pathname);
   for(int i = 0; i < LENGTH(file_table); i++) {
     if(strcmp(pathname, file_table[i].name) == 0) {

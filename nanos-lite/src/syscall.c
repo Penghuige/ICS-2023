b5e018f8b49f51075268639310c388d303d38075
tracer-ics2023 <Penghui@Linux>
2024-07-16 13:01:37 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:01:37 up 6 days,  2:19,  1 user,  load average: 0.24, 0.28, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -37,7 +37,7 @@ void do_syscall(Context *c) {
       c->GPRx = 0;
       break;
     case 2: // sys_open
-      path = (char*)a[1];
+      path = "/bin/file-tests";
       //printf("path: %s\n", path);
       c->GPRx = fs_open(path, a[2], a[3]);
       break;

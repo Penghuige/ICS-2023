eeacca11a51a59621cda3b0d3e039cab8e754a3d
tracer-ics2023 <Penghui@Linux>
2024-07-16 17:11:59 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:11:59 up 6 days,  6:29,  1 user,  load average: 0.38, 0.55, 1.69

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -12,11 +12,12 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-// Record open-file table, it needs to store file name and offset
+// record open-file table, it need to store file name and offset
 typedef struct {
   size_t fd;
-  size_t open_offset; // Where it must write
-} OFinfo;
+  // where it must to write
+  size_t open_offset;
+}OFinfo;
 
 // File system
 int fs_open(const char *pathname, int flags, int mode);
@@ -25,7 +26,7 @@ size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
 int fs_close(int fd);
 
-enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
@@ -48,99 +49,126 @@ static Finfo file_table[] __attribute__((used)) = {
 #include "files.h"
 };
 
-// It is not needed to use in other files
-static int open_index = 0; // Explicit initialization
+// it is not need to use in other files
+static int open_index;
 static OFinfo open_table[LENGTH(file_table)];
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  open_index = 0; // Reset open_index
+  open_index = 0;
 }
 
-static int get_index(int fd) {
-  if (fd < 0 || fd >= LENGTH(open_table)) {
+static int get_index(int fd)
+{
+  if (fd < 0 || fd >= LENGTH(open_table))
+  {
     return -1;
   }
-  if (fd <= 2) {
+  if(fd <= 2)
+  {
     Log("ignore open %s", file_table[fd].name);
     return fd;
   }
-  for (int i = 0; i < open_index; i++) {
-    if (fd == open_table[i].fd) {
-      return i;
+  int ret = -1;
+  for(int i = 0; i < open_index; i++)
+  {
+    //printf("name: %d\n", open_table[i].fd);
+    if(fd == open_table[i].fd)
+    {
+      ret = i;
+      break;
     }
   }
-  return -1;
+  return ret;
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
   Log("open %s", pathname);
-  for (int i = 0; i < LENGTH(file_table); i++) {
-    if (strcmp(pathname, file_table[i].name) == 0) {
-      if (i <= 2) {
+  for(int i = 0; i < LENGTH(file_table); i++) {
+    printf("file_table[%d].name: %s\n", i, file_table[i].name);
+    if(strcmp(pathname, file_table[i].name) == 0) {
+      if(i <= 2)
+      {
         Log("ignore open %s", pathname);
         return i;
       }
-      // Record
+      // record 
+      //printf("file fd is %d\n", i);
+      //printf("open_index is %d\n", open_index);
       assert(open_index < LENGTH(open_table));
       open_table[open_index].fd = i;
       open_table[open_index].open_offset = 0;
       open_index++;
+
       return i;
     }
   }
   panic("file %s not found", pathname);
-  while (1);
+  while(1);
   return -1;
 }
 
 int fs_close(int fd) {
   int index = get_index(fd);
-  if (index != -1) {
-    open_table[index].fd = -1; // Fixed: use index instead of fd
+  if(index != -1)
+  {
+    open_table[fd].fd = -1;
   }
   return 0;
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
   int index = get_index(fd);
-  Log("READ index: %d, name: %s, offset: %zu", index, file_table[fd].name, open_table[index].open_offset);
-  if (index == -1) {
+  Log("READ index: %d, name: %s, offset: %d", index, file_table[fd].name, open_table[index].open_offset);
+  if(index == -1)
+  {
     panic("file %d not found", fd);
   }
   size_t offset = open_table[index].open_offset;
+  // printf("offset: %d\n", offset);
+  // 他的buf就是指向的Elf_Ehdr ehdr, 第二个参数是偏移量，记录在文件表中的disk_offset，第三个参数是长度
   size_t read_len = len;
+  // when the file size is not enough
   if (file_table[fd].size < offset + len) {
     read_len = file_table[fd].size - offset;
   }
   size_t ret = ramdisk_read(buf, file_table[fd].disk_offset + offset, read_len);
+  // 怎么能用不知道的函数来写呢？
+  // size_t ret = file_table[fd].read(buf, offset, len);
+  printf("read ret: %d\n", ret);
   open_table[index].open_offset += ret;
   return ret;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   int index = get_index(fd);
-  Log("WRITE index: %d, name: %s, offset: %zu", index, file_table[fd].name, open_table[index].open_offset);
-  if (index == -1) {
+  Log("WRITE index: %d, name: %s, offset: %d", index, file_table[fd].name, open_table[index].open_offset);
+  if(index == -1)
+  {
     panic("file %d not found", fd);
   }
   size_t offset = open_table[index].open_offset;
-  size_t write_len = len;
-  if (file_table[fd].size < offset + len) {
-    write_len = file_table[fd].size - offset;
+  size_t read_len = len;
+  // when the file size is not enough
+  if(file_table[fd].size < offset + len)
+  {
+    read_len = file_table[fd].size - offset;
   }
-  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + offset, write_len);
+  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + offset, read_len);
+
   open_table[index].open_offset += ret;
   return ret;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  Log("LSEEK offset: %zu, whence: %d", offset, whence);
+  Log("LSEEK offset: %d, whence: %d", offset, whence);
   int index = get_index(fd);
-  if (index == -1) {
+  if(index == -1)
+  {
     panic("file %d not found", fd);
   }
-  switch (whence) {
+  switch(whence)
+  {
     case SEEK_SET:
       open_table[index].open_offset = offset;
       break;
@@ -155,4 +183,3 @@ size_t fs_lseek(int fd, size_t offset, int whence) {
   }
   return open_table[index].open_offset;
 }
-

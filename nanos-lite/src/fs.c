a3e16116bb6af710aca657d765010445da20e379
tracer-ics2023 <Penghui@Linux>
2024-07-16 14:39:28 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:39:28 up 6 days,  3:57,  1 user,  load average: 0.47, 0.50, 0.60

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -109,6 +109,31 @@ int fs_close(int fd) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
+    if (fd <= 2) {
+        Log("ignore read %s", file_table[fd].name);
+        return 0;
+    }
+
+    int target_index = get_index(fd);
+    if (target_index == -1) {
+        Log("file %s not open before read", file_table[fd].name);
+        return -1;
+    }
+
+    size_t read_len = len;
+    size_t open_offset = open_table[target_index].open_offset;
+    size_t size = file_table[fd].size;
+    size_t disk_offset = file_table[fd].disk_offset;
+
+    if (open_offset > size) return 0;
+
+    if (open_offset + len > size) read_len = size - open_offset;
+    ramdisk_read(buf, disk_offset + open_offset, read_len);
+    open_table[target_index].open_offset += read_len;
+    return read_len;
+}
+
+size_t my_fs_read(int fd, void *buf, size_t len) {
   int index = get_index(fd);
   Log("READ index: %d, name: %s, offset: %d", index, file_table[fd].name, open_table[index].open_offset);
   if(index == -1)

3f5a4e4e623cc6f3c7020f5cf88e8e798180b32f
tracer-ics2023 <Penghui@Linux>
2024-07-18 10:23:34 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:23:34 up  1:58,  1 user,  load average: 0.80, 0.85, 0.96

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,6 +21,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < ARRLEN(ref_r->gpr); i++)
   {
     if(cpu.gpr[i] != ref_r->gpr[i]){
+      printf("\e[1;31m$pc\t0x%x\n\e[0m", ref_r->pc);
       extern const char *regs[];
       printf("\e[1;31mthe different register is regs %s, dut is %08x, ref is %08x\n", regs[i], cpu.gpr[i], ref_r->gpr[i]);
       // disaplay all the right register
@@ -32,7 +33,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
         }
         printf("\n");
       }
-      printf("\e[1;31mtvec\t0x%08x\tmepc\t0x%08x\tmcause\t0x%08x\tmstatus\t0x%08x\n\e[0m", ref_r->csr.mtvec, ref_r->csr.mepc, ref_r->csr.mcause, ref_r->csr.mstatus);
+      printf("\e[1;31mmtvec\t0x%08x\tmepc\t0x%08x\tmcause\t0x%08x\tmstatus\t0x%08x\n\e[0m", ref_r->csr.mtvec, ref_r->csr.mepc, ref_r->csr.mcause, ref_r->csr.mstatus);
       return false;
     }
   }

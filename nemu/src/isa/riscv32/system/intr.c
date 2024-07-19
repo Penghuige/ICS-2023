df07b44f44803c38891fe8490c31f26d6d25f485
tracer-ics2023 <Penghui@Linux>
2024-07-19 11:13:48 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:13:48 up 1 day,  2:48,  1 user,  load average: 0.79, 0.68, 0.81

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,7 +19,7 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr.mcause = NO;
   // false instruction
-  cpu.csr.mepc = epc;
+  cpu.csr.mepc = epc + 4;
 
   // mstatus
   cpu.csr.mpp = cpu.csr.prv;

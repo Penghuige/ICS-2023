b9fd8e6d8105c5754d607b86781d0e856c5cf7c4
tracer-ics2023 <Penghui@Linux>
2024-07-19 13:04:26 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:04:26 up 1 day,  4:39,  1 user,  load average: 0.33, 0.39, 0.42

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,6 +14,9 @@ Context* __am_irq_handle(Context *c) {
       if (c->GPR1 == -1)
       {
         ev.event = EVENT_YIELD;
+        printf("pre mepc: 0x%x\n", c->mepc);
+        c->mepc += 4;
+        printf("after mecp: 0x%x\n", c->mepc);
       }
       else
         ev.event = EVENT_SYSCALL;
@@ -21,12 +24,6 @@ Context* __am_irq_handle(Context *c) {
       default: ev.event = EVENT_ERROR; break;
     }
 
-    if(c->GPR1 == -1 || 1)
-    {
-      printf("pre mepc: 0x%x\n", c->mepc);
-      c->mepc += 4;
-      printf("after mecp: 0x%x\n", c->mepc);
-    }
     c = user_handler(ev, c);
     assert(c != NULL);
   }
--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -52,7 +52,7 @@ static vaddr_t *csr_register(word_t imm)
 #define immB() do { *imm = SEXT((BITS(i, 31, 31) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1), 13); } while(0)
 #define immJ() do { *imm = SEXT((BITS(i, 31, 31) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1), 21); } while(0)
 #define immR() do { /* No immediate value for R-type instructions */ } while(0)
-#define ECALL(dnpc) do { getchar(); dnpc = isa_raise_intr(cpu.csr.prv == PRV_M ? MCAUSE_ECALL_FROM_M:MCAUSE_ECALL_FROM_U, s->pc); } while(0)
+#define ECALL(dnpc) do { dnpc = isa_raise_intr(cpu.csr.prv == PRV_M ? MCAUSE_ECALL_FROM_M:MCAUSE_ECALL_FROM_U, s->pc); } while(0)
 #define CSR(i) *csr_register(i)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {

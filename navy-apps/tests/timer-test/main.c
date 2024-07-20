1790cece8e529702a513feced698abda1d27fa1a
tracer-ics2023 <Penghui@Linux>
2024-07-20 12:11:52 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:11:52 up  2:47,  1 user,  load average: 0.65, 0.54, 0.42

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -3,23 +3,42 @@
 #include <sys/time.h>
 
 extern int _gettimeofday(struct timeval *tv, struct timezone *tz);
+extern uint32_t NDL_GetTicks();
 
-int main() {
-  struct timeval tv;
-  struct timeval pre;
-  _gettimeofday(&tv, NULL);
+//int main() {
+//  struct timeval tv;
+//  struct timeval pre;
+//  _gettimeofday(&tv, NULL);
+//  while(1)
+//  {
+//    pre = tv;
+//    _gettimeofday(&tv, NULL);
+//    __uint64_t ms = 500;
+//    if(tv.tv_usec - pre.tv_usec > ms)
+//    {
+//      printf("hello! now is %ld not %ld\n", tv.tv_usec, pre.tv_usec);
+//    }
+//    printf("now is %ld not %ld\n", tv.tv_usec, pre.tv_usec);
+//
+//    printf("tv.tv_sec = %ld\n", tv.tv_sec);
+//  }
+//
+//  return 0;
+//}
+//
+int main()
+{
+  uint32_t t1 = NDL_GetTicks();
+  uint32_t t2 = t1;
   while(1)
   {
-    pre = tv;
-    _gettimeofday(&tv, NULL);
-    __uint64_t ms = 500;
-    if(tv.tv_usec - pre.tv_usec > ms)
+    t2 = t1;
+    t1 = NDL_GetTicks();
+    // ms
+    if(t1 - t2 > 500)
     {
-      printf("hello! now is %ld not %ld\n", tv.tv_usec, pre.tv_usec);
+      printf("i will be print!\n");
     }
-    printf("now is %ld not %ld\n", tv.tv_usec, pre.tv_usec);
-
-    printf("tv.tv_sec = %ld\n", tv.tv_sec);
   }
 
   return 0;

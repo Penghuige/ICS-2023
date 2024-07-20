1c80fcfcba59205a84d54e65535e6d025ecf0124
tracer-ics2023 <Penghui@Linux>
2024-07-20 21:01:16 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:01:16 up 11:37,  1 user,  load average: 0.87, 0.84, 0.88

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -35,6 +35,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   // print into buf
   AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
+  printf("cfg.width is %d, cfg.height is %d\n", cfg.width, cfg.height);
   return snprintf(buf, len, "WIDTH: %d\nHEIGHT: %d\n", cfg.width, cfg.height);
 }
 

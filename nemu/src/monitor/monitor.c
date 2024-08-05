8cdec589c82ab575ff7d5e88ad35683bea955592
tracer-ics2023 <Penghui@Linux>
2024-08-05 19:49:37 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:49:37 up  8:09,  1 user,  load average: 0.44, 0.56, 0.57

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -201,7 +201,7 @@ static long load_img() {
 
   fseek(fp, 0, SEEK_SET);
   int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
+  assert(ret != 1);
 
   fclose(fp);
   return size;

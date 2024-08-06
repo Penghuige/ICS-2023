1910691c5544031a15155dbd906d0273c6761350
tracer-ics2023 <Penghui@Linux>
2024-08-06 18:29:50 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:29:50 up  8:14,  1 user,  load average: 1.81, 1.53, 1.75

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -36,9 +36,8 @@ int SDL_WaitEvent(SDL_Event *ev) {
 #define MIN(a, b) (a < b ? a : b)
 int SDL_PollEvent(SDL_Event *ev) {
   char* buf = malloc(4096);
-  printf("strlen size is %ld\n", strlen(buf));
   // read out the event
-  if(NDL_PollEvent(buf, strlen(buf)))
+  if(NDL_PollEvent(buf, 20))
   {
     int sign = 0;
     // it has a event, judge it.

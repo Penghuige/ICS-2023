cf40e87e08796d6639fb6b1610d10557cf36ff69
tracer-ics2023 <Penghui@Linux>
2024-08-07 20:03:52 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:03:52 up  9:25,  1 user,  load average: 0.27, 0.43, 0.63

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -132,6 +132,7 @@ int fs_open(const char *pathname, int flags, int mode) {
       assert(open_index < LENGTH(open_table));
       open_table[open_index].fd = i;
       open_table[open_index].open_offset = 0;
+      printf("open_index is %d\n", open_index);
       open_index++;
       Log("have read %s", pathname);
 

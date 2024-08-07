6b04ddf2525e2838bed87a9116115aac4b25dc93
tracer-ics2023 <Penghui@Linux>
2024-08-07 20:11:28 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:11:28 up  9:32,  1 user,  load average: 0.52, 0.49, 0.58

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -98,9 +98,10 @@ static int get_index(size_t fd)
   int ret = -1;
   for(int i = 0; i < open_index; i++)
   {
-    printf("now find name: %d\n", open_table[i].fd);
+    printf("now get name: %d\n", open_table[i].fd);
     if(fd == open_table[i].fd)
     {
+      printf("allright!\n");
       ret = i;
       break;
     }

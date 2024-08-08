8b6103380536291d92551524916bec8815a6ddc5
tracer-ics2023 <Penghui@Linux>
2024-08-08 12:18:24 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:18:24 up  3:12,  1 user,  load average: 0.30, 0.51, 0.57

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -12,6 +12,7 @@ bool ioe_init() {
 }
 
 void ioe_read (int reg, void *buf) {
+  // give the reg value.
   // support some io read
   printf("ioe_read reg is %d\n", reg);
   switch (reg) {
@@ -29,12 +30,8 @@ void ioe_read (int reg, void *buf) {
 void ioe_write(int reg, void *buf) {
   printf("ioe_write reg is %d\n", reg);
   switch (reg) {
-    case AM_GPU_CONFIG:
-      ((AM_GPU_CONFIG_T *)buf)->width = w;
-      ((AM_GPU_CONFIG_T *)buf)->height = h;
-      break;
-    case AM_TIMER_CONFIG: 
-      ((AM_TIMER_UPTIME_T *)buf)->us = NDL_GetTicks();
+    case AM_GPU_FBDRAW:
+      NDL_DrawRect(((AM_GPU_FBDRAW_T *)buf)->pixels, ((AM_GPU_FBDRAW_T *)buf)->x, ((AM_GPU_FBDRAW_T *)buf)->y, ((AM_GPU_FBDRAW_T *)buf)->w, ((AM_GPU_FBDRAW_T *)buf)->h);
       break;
     default:
       assert(0);

e5942cece24ede62c72d3d3816b8200163385076
tracer-ics2023 <Penghui@Linux>
2024-08-08 12:38:08 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:38:08 up  3:31,  1 user,  load average: 0.52, 0.69, 0.63

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -2,6 +2,7 @@
 #include <NDL.h>
 #include "amdev.h"
 #include <assert.h>
+#include <string.h>
 
 int w, h;
 
@@ -10,6 +11,13 @@ bool ioe_init() {
   NDL_OpenCanvas(&w, &h);
   return true;
 }
+
+#define KEY_NAMES(x) #x,
+
+static const char *keynames[] = {
+  "NONE",
+  AM_KEYS(KEY_NAMES)
+};
 
 void ioe_read (int reg, void *buf) {
   // give the reg value.
@@ -20,8 +28,41 @@ void ioe_read (int reg, void *buf) {
       ((AM_GPU_CONFIG_T *)buf)->width = w;
       ((AM_GPU_CONFIG_T *)buf)->height = h;
       break;
-    case AM_TIMER_CONFIG: 
+    case AM_TIMER_UPTIME:
       ((AM_TIMER_UPTIME_T *)buf)->us = NDL_GetTicks();
+      break;
+    case AM_TIMER_CONFIG: 
+      ((AM_TIMER_CONFIG_T *)buf)->present = true;
+      ((AM_TIMER_CONFIG_T *)buf)->has_rtc = true;
+      break;
+    case AM_INPUT_CONFIG:
+      ((AM_INPUT_CONFIG_T *)buf)->present = true;
+      break;
+    case AM_INPUT_KEYBRD:
+      // it need to get the keydown value.
+      // not value.
+      char ebuf[64];
+      if(0 == NDL_PollEvent((char *)ebuf, sizeof(ebuf))) {
+        *((AM_INPUT_KEYBRD_T *)buf) = (AM_INPUT_KEYBRD_T){.keydown=false, .keycode=0};
+        break;
+      }
+      // find who has been pressed.
+      bool keydown = ebuf[1] == 'd';
+      int cnt = -1;
+      for(int i = 0; i < sizeof(keynames)/sizeof(keynames[0]); i++) {
+        if (strncmp(ebuf + 3, keynames[i], strlen(keynames[i])) == 0)
+        {
+          cnt = i;
+          break;
+        }
+      }
+      if(cnt == -1)
+      {
+        printf("ebuf is %s, key not fonud!\n", ebuf);
+        assert(0);
+      }
+      ((AM_INPUT_KEYBRD_T *)buf)->keydown = keydown;
+      ((AM_INPUT_KEYBRD_T *)buf)->keycode = cnt;
       break;
     default:
       assert(0);

072be0e7e7abc279ae7646a8aad0d214acfb4229
tracer-ics2023 <Penghui@Linux>
2024-08-08 12:10:58 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:10:58 up  3:04,  1 user,  load average: 0.70, 0.64, 0.62

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,11 +1,23 @@
 #include <am.h>
+#include <NDL.h>
+#include "amdev.h"
 #include <assert.h>
 
+int w, h;
+
 bool ioe_init() {
+  NDL_Init(0);
+  NDL_OpenCanvas(&w, &h);
   return true;
 }
 
 void ioe_read (int reg, void *buf) {
+  // support some io read
+  printf("reg is %d\n", reg);
+  switch (reg) {
+    case AM_TIMER_CONFIG: 
+      break;
+  }
   assert(0);
 }
 void ioe_write(int reg, void *buf) {

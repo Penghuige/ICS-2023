59b5e502405672a609ba491461927b7c3d633a69
tracer-ics2023 <Penghui@Linux>
2024-08-09 16:29:02 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:29:02 up  6:13,  1 user,  load average: 0.17, 0.11, 0.10

--- navy-apps/tests/audio-test/main.c
+++ navy-apps/tests/audio-test/main.c
@@ -15,6 +15,8 @@ int main() {
   uint8_t *buf = malloc(len);
   fseek(f, 0, SEEK_SET);
   assert(len == fread(buf, 1, len, f));
+
+  printf("buf is %p, len is %d\n", buf, len);
   
   NDL_Init(0);
   NDL_OpenAudio(8000, 1, 1024);

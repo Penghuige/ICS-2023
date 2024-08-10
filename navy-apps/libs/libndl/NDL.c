34b3665c952af6bf8b1a8bd736b0f2d34d7f8fb2
tracer-ics2023 <Penghui@Linux>
2024-08-10 10:31:42 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:31:42 up 1 day, 16 min,  1 user,  load average: 0.39, 0.65, 0.62

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -104,14 +104,6 @@ static void init_display() {
   char *tmp;
   int nread = read(dispinfo, buf, sizeof(buf));
   assert(nread > 0 && nread < sizeof(buf));
-  for (tmp = strtok(buf, "\n"); tmp; tmp = strtok(NULL, "\n")) {
-    if (1 == sscanf(tmp, "WIDTH:%d", &screen_w)) continue;
-    if (1 == sscanf(tmp, "HEIGHT:%d", &screen_h)) continue;
-    tmp += strspn(tmp, " \t");
-    if (tmp[0] == '#' || tmp[0] == '\0') continue;
-    fprintf(stderr, "/proc/dispinfo: syntax error near '%s'\n", tmp);
-    assert(0);
-  }
 }
 
 static void init_audio() {

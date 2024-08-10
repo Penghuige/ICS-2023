dfbf67834c297eaff2779a4a6ea8d411d0e398df
tracer-ics2023 <Penghui@Linux>
2024-08-10 23:18:03 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:18:03 up 1 day, 13:02,  1 user,  load average: 0.82, 0.98, 0.92

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -8,9 +7,13 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
-#define AUDIO_PLAY     (AUDIO_ADDR + 0x18)
+
+static uint32_t pos = 0;
 
 void __am_audio_init() {
+  //outl(AUDIO_FREQ_ADDR, 0);
+  //outl(AUDIO_CHANNELS_ADDR, 0);
+  //outl(AUDIO_SAMPLES_ADDR, 0);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
@@ -18,10 +21,13 @@ void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
+
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  // here is out, not in.
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
-  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  // tell the nemu, it needs init 
   outl(AUDIO_INIT_ADDR, 1);
 }
 
@@ -29,14 +35,17 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
   stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
-/* Block function
- * make sure [ctl->start, ctl->end) is appended in sound buffer
- */
+
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  uintptr_t start = (uintptr_t)ctl->buf.start;
-  uintptr_t end = (uintptr_t)ctl->buf.end;
-  outl(AUDIO_PLAY, start & 0xffffffff);
-  outl(AUDIO_PLAY + 4, ((uint64_t) start) >> 32);
-  outl(AUDIO_PLAY + 8, end & 0xffffffff);
-  outl(AUDIO_PLAY + 12, ((uint64_t) end) >> 32);
+  uint8_t* audio_data = ctl->buf.start;
+  uint32_t len = ctl->buf.end - ctl->buf.start;
+
+  uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
+  
+  uint8_t *ab = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;  //参考GPU部分
+  for(int i = 0; i < len; i++){
+    ab[pos] = audio_data[i];
+    pos = (pos + 1) % sbuf_size;  
+  }
+  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len); //更新reg_count
 }

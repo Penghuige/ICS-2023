ce315f320066ab43781eab936c1ecda1e06f2f0b
tracer-ics2023 <Penghui@Linux>
2024-08-10 21:46:47 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:46:47 up 1 day, 11:31,  1 user,  load average: 1.12, 1.10, 1.22

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,7 +35,7 @@ void init_proc() {
   //naive_uload(NULL, filename3);
 
   //const char filename4[] = "/bin/nplayer";
-  const char filename4[] = "/bin/bmp-test";
+  const char filename4[] = "/bin/bird";
   naive_uload(NULL, filename4);
 
 }

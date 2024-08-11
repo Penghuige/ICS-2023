d19794c10c0a264c7cc67625834717396e8f8483
tracer-ics2023 <Penghui@Linux>
2024-08-11 20:43:14 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-40-generic #40-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 10:34:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:43:14 up  8:50,  1 user,  load average: 0.56, 0.67, 0.69

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,9 +55,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  [FD_STDIN]  = {"stdin", 0, 0, NULL, NULL},
+  [FD_STDOUT] = {"stdout", 0, 0, NULL, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, NULL, serial_write},
   [DEV_EVENTS] = {"/dev/events", 0, 0, events_read, NULL},
   [DEV_DISPLAY] = {"/proc/dispinfo", 0, 0, dispinfo_read, NULL},
   [FD_FB] = {"/dev/fb", 0, 0, NULL, fb_write},

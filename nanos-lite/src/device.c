716fd528f606a7f7b1b9402754bc7a78dd3d0aea
tracer-ics2023 <Penghui@Linux>
2024-08-11 14:29:03 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-40-generic #40-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 10:34:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:29:03 up  2:36,  1 user,  load average: 0.38, 0.40, 0.42

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -54,6 +54,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t sb_write(const void *buf, size_t offset, size_t len) {
+  printf("buf = %p, offset = %d, len = %d\n", buf, offset, len);
   printf("[sb_write] write to AM_AUDIO_PLAY, offset = %d, len = %d\n", offset, len);
   io_write(AM_AUDIO_PLAY, (Area){.start = (void *)buf, .end = (void *)buf + len});
 

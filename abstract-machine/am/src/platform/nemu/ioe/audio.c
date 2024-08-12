ee6059cb6974a43b7cfa44bdc8871c4c7517fde8
tracer-ics2023 <Penghui@Linux>
2024-08-12 13:48:38 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-40-generic #40-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 10:34:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:48:38 up  2:15,  1 user,  load average: 0.77, 0.66, 0.69

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
@@ -44,9 +45,14 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
   
   uint8_t *ab = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;  //参考GPU部分
-  for(uint32_t i = 0; i < len; i++){
-    ab[pos] = audio_data[i];
-    pos = (pos + 1) % sbuf_size;  
-  }
+                                                
+  
+  //for(uint32_t i = 0; i < len; i++){
+  //  ab[pos] = audio_data[i];
+  //  pos = (pos + 1) % sbuf_size;  
+  //}
+  memcpy(ab + pos, audio_data, len);
+  pos = (pos + len) % sbuf_size;
+
   outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len); //更新reg_count
 }

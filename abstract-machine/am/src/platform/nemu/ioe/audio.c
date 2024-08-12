5ccc478d2a5787825b88034592fe0751dbe8fb78
tracer-ics2023 <Penghui@Linux>
2024-08-12 13:35:26 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-40-generic #40-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 10:34:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:35:26 up  2:02,  1 user,  load average: 0.41, 0.50, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -47,6 +47,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   printf("audio_data: %x\n", audio_data);
   printf("ab: %x\n", ab);
   printf("len is %d\n", len);
+  printf("sbuf_size is %d\n", sbuf_size);
   for(uint32_t i = 0; i < len; i++){
     ab[pos] = audio_data[i];
     pos = (pos + 1) % sbuf_size;  

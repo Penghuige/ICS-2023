de171959a11161b326439b8424113835a39c9300
tracer-ics2023 <Penghui@Linux>
2024-08-15 00:09:13 +0800

    >  compile NEMU Penguhi 蓬灰 Linux Ulimate-PH 6.8.0-40-generic #40-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 10:34:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:09:13 up  7:14,  1 user,  load average: 0.93, 1.71, 3.70

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,8 +19,25 @@ extern size_t fs_read(int fd, void *buf, size_t len);
 extern size_t fs_write(int fd, void *buf, size_t len);
 extern size_t fs_lseek(int fd, size_t offset, int whence);
 
-
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // check valid elf
+  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      // set .bss with zeros
+      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+    }
+  }
+  return ehdr.e_entry;
+}
+
+ uintptr_t loader2(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   if (fd < 0) {
     Log("File not found: %s", filename);
